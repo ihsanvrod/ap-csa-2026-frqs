4717bac30489fa20bef1b027a43b60fe2e263c28
ihsanvrod <ihsanvrod@gmail.com>
2026-05-19 10:01:07 -0400

    Create partB.java

---
+++ question-1/partB.java
@@ -0,0 +1,11 @@
+/**
+* Returns a shortened version of username with each hyphen ("-")
+* and the character before it removed, as described in part (b)
+* Preconditions: username does not start or end with a hyphen.
+* username does not contain consecutive hyphens.
+* username.length() >= 2
+* Postcondition: username is unchanged.
+*/
+public String getShortenedName(){
+  //
+}
