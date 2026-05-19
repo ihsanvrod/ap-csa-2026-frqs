e6195a1aff8c68109bb431206e09d16c201f5ce9
ihsanvrod <ihsanvrod@gmail.com>
2026-05-19 09:54:20 -0400

    Update partA.java

--- question-1/partA.java
+++ question-1/partA.java
@@ -1,1 +1,9 @@
-
+public Account(String requestedName){
+  String name = requestedName;
+  int i = 1;
+  while(name.isAvailable()==false){
+      name = requestedName+Integer.toString(i);
+      i++;
+  }
+  username = name;
+}
